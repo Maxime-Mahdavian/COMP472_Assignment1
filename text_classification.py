2f0c65b90d0a430ec60fd1c7b87f6ca151a64239
Maxime Mahdavian <maxime.mahdavian2@gmail.com>
2021-09-17 16:05:22 -0400

    forgot to change something

--- text_classification.py
+++ text_classification.py
@@ -46,31 +46,16 @@ def prepare_datasets():
 
 def main():
     plot_distribution()
-    # files, train_set, test_set = prepare_datasets()
-    # print(train_set.shape)
-    # print(test_set.shape)
+    files, train_set, test_set = prepare_datasets()
+    print(train_set.shape)
+    print(test_set.shape)
 
-    files = load_files('BBC', encoding="latin1")
-
-    # Tokenize words
-    vectorizer = CountVectorizer()
-    x = vectorizer.fit_transform(files.data)
-
-    # Transforms the occurrence counts to frequencies of word
-    tf_transformer = TfidfTransformer()
-    x_train_tf = tf_transformer.fit_transform(x)
-    # print(x_train_tf.shape)
-
-    # Splits the data set for training and testing
-    train_set, test_set = model_selection.train_test_split(x, train_size=0.8,
-                                                           test_size=0.20,
-                                                        random_state=None)
-    y = np.zeros(int(x.shape[0] * 0.8))
+    y = np.zeros(int(files.shape[0] * 0.8))
     clf = MultinomialNB()
     clf.fit(train_set, y)
 
     prediction = clf.predict(test_set)
-    print(prediction)
+    #print(prediction)
 
 if __name__ == "__main__":
     main()
